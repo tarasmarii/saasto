5364fb4b568e8baab1805052339639f34621bb5f
tarasmarii <tarasmarii33@gmail.com>
2023-06-03 23:49:02 +0300

    some commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
-$('.switcher').click(function () {
-    $(this).toggleClass('on');
+"use strict";
+
+let switchBtn = document.querySelector('.switcher');
+switchBtn.addEventListener("click", () => {
+    switchBtn.classList.toggle("on");
 });
 
-$('.mobile__btn').click(function(){
-    $(this).toggleClass('active');
-    $('.nav').toggleClass('show');
-});
+let menuBtn = document.querySelector(".mobile__btn");
+let nav = document.querySelector(".nav");
+
+window.addEventListener("click", (e) => {
+      if (e.target == menuBtn & !nav.classList.contains("show")) {
+nav.classList.add('show');
+menuBtn.classList.add("active");
+      }
+      else if (e.target == nav) {
+        return;
+      }
+      else {
+        nav.classList.remove('show');
+        menuBtn.classList.remove("active");
+      }
+
+    });
